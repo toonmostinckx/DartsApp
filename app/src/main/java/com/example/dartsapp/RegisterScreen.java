fb6ad1a36adaac4e4442bcc369be7e67ac05aa93
GijsFitenKUL <gijs.fiten@student.kuleuven.be>
2021-05-06 15:17:39 +0200

    Handling wrong register info

--- app/src/main/java/com/example/dartsapp/RegisterScreen.java
+++ app/src/main/java/com/example/dartsapp/RegisterScreen.java
@@ -4,18 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.apache.commons.codec.binary.Hex;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
-import java.util.Arrays;
 import java.util.Random;
-
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
@@ -25,6 +25,8 @@ public class RegisterScreen extends AppCompatActivity implements View.OnClickLis
     EditText emailBox;
     EditText passwordBox;
     EditText repeatPasswordBox;
+    TextView errorMessageBox;
+
     Button registerButton;
     
     @Override
@@ -36,6 +38,7 @@ public class RegisterScreen extends AppCompatActivity implements View.OnClickLis
         emailBox = findViewById(R.id.RegisterScreen_EmailBox);
         passwordBox = findViewById(R.id.RegisterScreen_PassWordBox);
         repeatPasswordBox = findViewById(R.id.RegisterScreen_RepeatPassWordBox);
+        errorMessageBox = findViewById(R.id.RegisterScreen_ErrorMessageBox);
 
         registerButton = findViewById(R.id.RegisterScreen_RegisterButton);
         registerButton.setOnClickListener(this);
@@ -49,19 +52,31 @@ public class RegisterScreen extends AppCompatActivity implements View.OnClickLis
     }
     
     public void registerAccount(){
+        errorMessageBox.setText("");
         //check if email already has account linked
         if(checkExistingEmail(emailBox.getText().toString())){
+            String error = "Email already registered to a user";
+            errorMessageBox.setText(error);
+
+            //clear passwords
+            passwordBox.setText("");
+            repeatPasswordBox.setText("");
             return;
         }
         
         //check if passwords match
         if(!passwordBox.getText().toString().equals(repeatPasswordBox.getText().toString())){
+            String error = "Passwords do not match";
+            errorMessageBox.setText(error);
+
+            //clear passwords
+            passwordBox.setText("");
+            repeatPasswordBox.setText("");
             return;
         }
 
-        byte[] hashedPassword = null;
-
         //Hash the password
+        String hashedPassword;
         try {
             hashedPassword = hashPassword();
         }catch (Exception e){
@@ -83,25 +98,33 @@ public class RegisterScreen extends AppCompatActivity implements View.OnClickLis
         startActivity(intent);
     }
 
-    private byte[] hashPassword() throws NoSuchAlgorithmException, InvalidKeySpecException {
+    private String hashPassword() throws NoSuchAlgorithmException, InvalidKeySpecException {
         //generate Salt
         byte[] salt = generateSalt();
 
-        KeySpec spec = new PBEKeySpec(passwordBox.getText().toString().toCharArray(), salt, 32768, 64);
+        //generate bytearray of the hashed password
+        KeySpec spec = new PBEKeySpec(passwordBox.getText().toString().toCharArray(), salt, 65536, 128);
         SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        return factory.generateSecret(spec).getEncoded();
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+
+        //return string from bytearray using Apache commontools Hex
+        return Hex.encodeHexString(hash);
     }
 
+
     private byte[] generateSalt() {
+        //generates seed for PRNG from the users email
         long seed = generateLongFromString(emailBox.getText().toString());
         Random randomGen = new Random(seed);
 
+        //fills salt array with random bytes
         byte[] salt = new byte[16];
         randomGen.nextBytes(salt);
         return salt;
     }
 
     private long generateLongFromString(String givenString) {
+        //generates long from the users emails ASCII codes
         char[] chararray = givenString.toCharArray();
         long seed = 0;
         for (char c : chararray) {
